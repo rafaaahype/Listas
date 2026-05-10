8bc126f7d5bb89fd0719c8db7070e9b681c7a60f
rafaelchep <rafaelsvieira777@gmail.com>
2026-05-09 22:25:58 -0300

    testando struct

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "../bibliotecas/listaestatica.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 typedef struct {
 	char nome[100];
 	char cpf[15];
@@ -7,5 +8,11 @@ typedef struct {
 } Funcionario;
 
 int main(void){
+	Funcionario *f = (Funcionario*) malloc(sizeof(Funcionario));
+	if(f){
+		f->salario=3;
+		printf("%.2f\n",f->salario);
+		free(f);
+	}
 	return 0;
 }

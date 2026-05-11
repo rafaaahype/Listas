abaa43a509f3f75a26d9c3b33d066dbd72c1635c
JefferssonAllves <jeffersson.dev@gmail.com>
2026-05-10 23:07:00 -0300

    Finalização - Não testado 100%

--- src/encadeadas.c
+++ src/encadeadas.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 #include "../bibliotecas/listaEncadeada/listaencadeada.h"
-int main(){
-  printf("Jeffersson Kauan Alves de Lima");
+int main(void){
+  Funcionario* lista = criar_lista();
+
+  for(int i=0; i<3; i++){
+    Funcionario func;
+    printf("DIGITE O NOME DO FUNCIONARIO: ");
+    scanf("%s", func.nome);
+    printf("DIGITE O CPF DO FUNCIONARIO: ");
+    scanf("%s", func.cpf);
+    printf("DIGITE O SALARIO DO FUNCIONARIO: ");
+    scanf("%f", &func.salario);
+    lista = add_lista_ordenado(lista, func);
+
+
+  }
+  imprime_lista(lista);
+  lista = liberar_lista(lista);
+  imprime_lista(lista);
   return 0;
 }
